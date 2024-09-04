936c9cb48e3914140a056a5d75691e9f376c2cca
Adham Nour <adhamnourelwaffaa@gmail.com>
2024-09-04 23:25:45 +0300

    loaded files in DB

---
+++ important sql.sql
@@ -0,0 +1,31 @@
+-- parse date from file name
+
+SELECT
+	DISTINCT 
+	STR_TO_DATE(CONCAT(REGEXP_SUBSTR(file_name, '[0-9]+'), '01') ,
+	'%Y%m%d')
+FROM
+	credit_card_transaction_files cctf
+where
+	Transaction_Date is not null;
+
+-- replacing month apprev with number
+
+SELECT
+distinct value_date , CASE
+        WHEN value_date LIKE '%Jan%' THEN REPLACE(value_date, 'Jan', '01')
+        WHEN value_date LIKE '%Feb%' THEN REPLACE(value_date, 'Feb', '02')
+        WHEN value_date LIKE '%Mar%' THEN REPLACE(value_date, 'Mar', '03')
+        WHEN value_date LIKE '%Apr%' THEN REPLACE(value_date, 'Apr', '04')
+        WHEN value_date LIKE '%May%' THEN REPLACE(value_date, 'May', '05')
+        WHEN value_date LIKE '%Jun%' THEN REPLACE(value_date, 'Jun', '06')
+        WHEN value_date LIKE '%Jul%' THEN REPLACE(value_date, 'Jul', '07')
+        WHEN value_date LIKE '%Aug%' THEN REPLACE(value_date, 'Aug', '08')
+        WHEN value_date LIKE '%Sep%' THEN REPLACE(value_date, 'Sep', '09')
+        WHEN value_date LIKE '%Oct%' THEN REPLACE(value_date, 'Oct', '10')
+        WHEN value_date LIKE '%Nov%' THEN REPLACE(value_date, 'Nov', '11')
+        WHEN value_date LIKE '%Dec%' THEN REPLACE(value_date, 'Dec', '12')
+        ELSE value_date
+    END AS updated_value_date
+FROM
+	credit_card_transaction_files cctf
--- single_file_processor.py
+++ single_file_processor.py
@@ -7,27 +7,20 @@ def load_file(host_name :str,database_name:str,username:str,password:str,file_pa
     df=pd.read_excel(io=file_path)
     index = df[df['Unnamed: 15'] == 'OPENING BALANCE'].index[0]+1
     df=df.loc[index:]
-    index = df[df['Unnamed: 15'] == 'CLOSING BALANCE'].index[0]-1
-    df=df.loc[:index]
     df = df.iloc[:, [4, 8, 15, 33]]
-    df.columns = ["Transaction Date","Value Date","Description","Amount"]
+    df.columns = ["Transaction_Date","Value_Date","Description","Amount"]
     for column in df.columns:
         if column != 'Amount':
-            df[column] = df[column].fillna(method='ffill')
+            df[column] = df[column].ffill()
     df = df[df['Amount'].notna()]
-    if not df.empty:
-        df['Amount']=df['Amount'].str.strip()
-        df[['Transaction_Amount', 'sign']] = df["Amount"].str.split(" ",n=1, expand=True)
-        df['Transaction_Amount']=df['Transaction_Amount'].astype('float')
-        df['signed_amount'] = df['Transaction_Amount'].where(~df['sign'].isnull(), -1 * df['Transaction_Amount'])
-        df = df.drop(columns=['Amount', 'Transaction_Amount', 'sign'])
-        filename = os.path.basename(file_path)
-        stmt_datetime_str =filename.split('.')[0].split('_')[1]+'01'
-        stmt_datetime=datetime.strptime(stmt_datetime_str, "%Y%m%d")
-        df['Statement Date']=stmt_datetime
-        df['Transaction Date']=df['Transaction Date']+f"-{stmt_datetime.year}"
-        df['Value Date']=df['Value Date']+f"-{stmt_datetime.year}"
-        df['Transaction Date']=pd.to_datetime(df['Transaction Date'],format='%d-%m-%Y')
-        df['Value Date']=pd.to_datetime(df['Value Date'],format='%d-%m-%Y')
-        engine = create_engine(f"mysql+pymysql://{username}:{password}@{host_name}/{database_name}")
-        df.to_sql("credit_cards_transactions", engine, if_exists="append", index=False)
+    df['Amount']=df['Amount'].astype('str').str.strip()
+    df[['Transaction_Amount', 'sign']] = df['Amount'].apply(
+        lambda x: x.split(" ", 1) if " " in x else [x, None]
+    ).apply(pd.Series)
+    df['Transaction_Amount']=df['Transaction_Amount'].astype('float')
+    df['signed_amount'] = df['Transaction_Amount'].where(~df['sign'].isnull(), -1 * df['Transaction_Amount'])
+    df = df.drop(columns=['Amount', 'Transaction_Amount', 'sign'])
+    filename = os.path.basename(file_path)
+    df['file_name']=filename
+    engine = create_engine(f"mysql+pymysql://{username}:{password}@{host_name}/{database_name}")
+    df.to_sql("credit_card_transaction_files", engine, if_exists="append", index=False)
